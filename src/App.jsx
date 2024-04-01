da018497bae5fb197fa414e523c1ba3b512dc332
Noman4d <nomanahmed.abdullah@gmail.com>
2024-04-01 15:59:37 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1 className='text-xl bg-red-400'>Ajke theika abar shuru
+     </h1>
+    </>
+  )
+}
+
+export default App
